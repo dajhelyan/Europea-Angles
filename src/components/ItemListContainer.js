e31b6a90455b5dab1e9ea52e0fae03a8ed048cb2
dajhelyan <dajhelyangle@gmail.com>
2021-06-29 13:44:09 -0500

    Llamado a promesa que retorna la data en un tiempo diferido en componente ItemListcontainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from 'react-bootstrap'
 import { ItemCount } from './ItemCount';
-
+import ItemList from './ItemList'
+import dataJSON from '../data/products'
 export function ItemListContainer() {
-	
+	// aca hacer promesa 
+	const [data, setData]= useState([])
+	const[loading, setLoading] = useState()
+
+	useEffect(() => {
+		new Promise((resolve, reject) => {
+		setTimeout(resolve(dataJSON), 3000);
+		}).then(res => {
+			console.log(res, 'gere');
+			setData(res)
+		}).catch(res => {
+			console.log(res)
+		})
+}, [])
+
 	return (
 		<Card>
 			<Card.Body>
-				<ItemCount stock={5} initial={1} onAdd={() => {alert("Producto(s) agregados")}}/>
+				{/* <ItemCount stock={5} initial={1} onAdd={(count) => {alert(count + "Producto(s) agregados" )}}/> */}
+				<ItemList items={data}/>
 			</Card.Body>
 		</Card>
     )
